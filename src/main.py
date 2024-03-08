3d58c3758cc139a5164e57113007f7cd4e046d06
Popov Stas <boss.staspopov@mail.ru>
2024-03-08 16:05:03 +0300

    Добавил middleware

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi_cache import FastAPICache
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
 from auth.base_config import auth_backend, fastapi_users
@@ -26,6 +27,18 @@ app.include_router(
 app.include_router(router_operation)
 app.include_router(router_tasks)
 
+origins = [
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
 
 @app.on_event("startup")
 async def startup_event():
--- src/operations/router.py
+++ src/operations/router.py
@@ -15,7 +15,6 @@ router = APIRouter(
 )
 
 
-
 @router.get('/')
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
     try:
