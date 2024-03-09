d31d8e2e0c546d88c38e8ef5a839c76346b0ef58
Popov Stas <boss.staspopov@mail.ru>
2024-03-09 18:47:21 +0300

    Создал модель Messages

--- migrations/env.py
+++ migrations/env.py
@@ -11,7 +11,8 @@ import sys
 sys.path.append(os.path.join(sys.path[0], 'src'))
 
 from src.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
-from src.database import metadata
+from src.database import metadata, Base
+from src.chat.models import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -33,7 +34,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = [metadata, Base.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ src/chat/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, String, Integer
+from database import Base
+
+class Message(Base):
+    __tablename__ = 'messages'
+    id = Column(Integer, primary_key=True)
+    message = Column(String)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
